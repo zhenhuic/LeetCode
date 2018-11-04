5e14186940160c185dd175eb3e21ddea1db58ea1
zhenhuic <zhenhuiccc@gmail.com>
2018-11-04 21:09:52 +0800

    Update array_easy.py

--- array_easy.py
+++ array_easy.py
@@ -205,11 +205,13 @@ class Solution(object):
         front_sum, rear_sum= nums[front], nums[rear]
         while front < rear:
             if front_sum < rear_sum:
-                front_sum += nums[++front]
+                front += 1
+                front_sum += nums[front]
             elif front_sum == rear_sum and rear - front == 2:
                 return front + 1
             else:
-                rear_sum += nums[--rear]
+                rear -= 1
+                rear_sum += nums[rear]
         return -1
 
     def isOneBitCharacter(self, bits):
