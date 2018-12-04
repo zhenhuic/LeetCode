5a9bc00cd2760a4266a10a1adada0238145200e0
zhenhuic <zhenhuiccc@gmail.com>
2018-12-04 21:33:04 +0800

    2018.12.4

--- algorithm/array_easy.py
+++ algorithm/array_easy.py
@@ -481,4 +481,19 @@ class Solution(object):
 
         reverse(nums, i + 1)
 
+    def hasGroupsSizeX(self, deck):
+        """
+        :type deck: List[int]
+        :rtype: bool
+        """
+        count = collections.Counter(deck)
+        N = len(deck)
+        for X in range(2, N + 1):
+            if N % X == 0:
+                if all(v % X == 0 for v in count.values()):
+                    return True
+        return False
+
+        
+
 
