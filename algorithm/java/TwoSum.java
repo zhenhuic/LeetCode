779c762a47031096441714921307f63731f4c046
Zhenhui Chen <zhenhuiccc@gmail.com>
2020-05-06 19:52:11 +0800

    1. 两数之和

---
+++ algorithm/java/TwoSum.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 1. 两数之和
+ */
+public class TwoSum {
+    /**
+     * 利用 Map 快速找到 target - nums[i] 值对应的索引
+     */
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int n = nums[i];
+            if (map.containsKey(target - n)) {
+                return new int[]{map.get(target - n), i};
+            } else {
+                map.put(nums[i], i);
+            }
+        }
+        return new int[0];
+    }
+}
