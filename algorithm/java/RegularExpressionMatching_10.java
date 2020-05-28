d6b846bae39b8a7180c14608886f329076009f83
Zhenhui Chen <zhenhuiccc@gmail.com>
2020-05-28 20:42:24 +0800

    10 DP

--- algorithm/java/RegularExpressionMatching_10.java
+++ algorithm/java/RegularExpressionMatching_10.java
@@ -1,32 +1,41 @@
 /**
  * 10. 正则表达式匹配
  */
-public class RegularExpressionMatching_10 {
+class RegularExpressionMatching_10 {
+    /**
+     * 动态规划
+     * 视频讲解 https://www.bilibili.com/video/BV1Tt4y1U7QP
+     */
     public boolean isMatch(String s, String p) {
         if (s == null || p == null) return false;
-        if (s.length() == 0 && p.length() == 0) return true;
-        if (s.length() != 0 && p.length() == 0) return false;
-        int strIdx = 0, patIdx = 0;
-        return matchCore(s, strIdx, p, patIdx);
-    }
 
-    private boolean matchCore(String s, int strIdx, String p, int patIdx) {
-        if (strIdx == s.length() && patIdx == p.length()) return true;
-        if (strIdx != s.length() && patIdx == p.length()) return false;
+        // memo[i][j] 代表 s[0..i] 与 p[0..j] 的匹配状态
+        boolean[][] memo = new boolean[s.length() + 1][p.length() + 1];
+        memo[0][0] = true;
 
-        if (patIdx + 1 < p.length() && p.charAt(patIdx + 1) == '*') {
-            if (strIdx < s.length() && (s.charAt(strIdx) == p.charAt(patIdx) || p.charAt(patIdx) == '.')) {
-                return matchCore(s, strIdx, p, patIdx + 2) ||
-                        matchCore(s, strIdx + 1, p, patIdx) ||
-                        matchCore(s, strIdx + 1, p, patIdx + 2);
-            } else {
-                return matchCore(s, strIdx, p, patIdx + 2);
+        // 初始化当 s 为空字符串时，memo[0][0..p.length()] 的匹配状态
+        for (int i = 0; i < p.length(); i++) {
+            if (p.charAt(i) == '*' && memo[0][i - 1]) {
+                memo[0][i + 1] = true;
             }
         }
 
-        if (strIdx < s.length() && s.charAt(strIdx) == p.charAt(patIdx) || p.charAt(patIdx) == '.') {
-            return matchCore(s, strIdx + 1, p, patIdx + 1);
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = 0; j < p.length(); j++) {
+                if (s.charAt(i) == p.charAt(j) || p.charAt(j) == '.') {
+                    memo[i + 1][j + 1] = memo[i][j];
+                }
+
+                if (p.charAt(j) == '*') {
+                    if (s.charAt(i) != p.charAt(j - 1) && p.charAt(j - 1) != '.') {
+                        memo[i + 1][j + 1] = memo[i + 1][j - 1];  // 表示 * 前面的字符出现 0 次
+                    } else {
+                        // 这里需要去看图表理解
+                        memo[i + 1][j + 1] = memo[i][j + 1] || memo[i + 1][j] || memo[i + 1][j - 1];
+                    }
+                }
+            }
         }
-        return false;
+        return memo[s.length()][p.length()];
     }
 }
