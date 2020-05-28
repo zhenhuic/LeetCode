44dbb46cf248b9b35531b5345d5f5ba8d2e8b578
Zhenhui Chen <zhenhuiccc@gmail.com>
2020-05-28 22:20:27 +0800

    3 HashMap 优化

--- algorithm/java/LongestSubstringWithoutRepeatingCharacters_3.java
+++ algorithm/java/LongestSubstringWithoutRepeatingCharacters_3.java
@@ -1,27 +1,24 @@
-import java.util.HashSet;
+import java.util.HashMap;
 
 /**
  * 3. 无重复字符的最长子串
  */
 class LongestSubstringWithoutRepeatingCharacters_3 {
     /**
-     * 用一个 HashSet 做滑动窗口
+     * 用一个 HashMap 做滑动窗口
      */
     public int lengthOfLongestSubstring(String s) {
-        if (s == null) return 0;
-        HashSet<Character> set = new HashSet<>();
-        int maxLen = 0;
-        int rightIndex = 0;
+        if (s.length() <= 1) return s.length();
+        HashMap<Character, Integer> map = new HashMap<>();
+        int max = 0;
+        int left = 0;
         for (int i = 0; i < s.length(); i++) {
-            if (i != 0) {
-                set.remove(s.charAt(i - 1));
+            if (map.containsKey(s.charAt(i))) {
+                left = Math.max(left, map.get(s.charAt(i)) + 1);  // 注意这里需要加 1
             }
-            while (rightIndex < s.length() && !set.contains(s.charAt(rightIndex))) {
-                set.add(s.charAt(rightIndex));
-                rightIndex++;
-            }
-            if (set.size() > maxLen) maxLen = set.size();
+            map.put(s.charAt(i), i);
+            max = Math.max(max, i - left + 1);
         }
-        return maxLen;
+        return max;
     }
 }
