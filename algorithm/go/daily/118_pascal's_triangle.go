9b3b1874b80b3cea7cbec3bda711a1deee540e68
Zhenhui Chen <zhenhuiccc@gmail.com>
2020-12-06 12:20:01 +0800

    20201206

---
+++ algorithm/go/daily/118_pascal's_triangle.go
@@ -0,0 +1,15 @@
+package main
+
+// 118. 杨辉三角
+func generate(numRows int) [][]int {
+	ans := make([][]int, numRows)
+	for i := 0; i < numRows; i++ {
+		ans[i] = make([]int, i+1)
+		ans[i][0] = 1
+		ans[i][i] = 1
+		for j := 1; j < i; j++ {
+			ans[i][j] = ans[i-1][j-1] + ans[i-1][j]
+		}
+	}
+	return ans
+}
