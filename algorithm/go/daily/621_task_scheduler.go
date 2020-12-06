7292fe4cf828378fd0f6b95681f37ccabb77d3da
Zhenhui Chen <zhenhuiccc@gmail.com>
2020-12-06 12:20:09 +0800

    20201205

---
+++ algorithm/go/daily/621_task_scheduler.go
@@ -0,0 +1,23 @@
+package main
+
+// 621. 任务调度器
+func leastInterval(tasks []byte, n int) int {
+	cnt := map[byte]int{}
+	for _, t := range tasks {
+		cnt[t]++
+	}
+
+	maxExec, maxExecCnt := 0, 0
+	for _, c := range cnt {
+		if c > maxExec {
+			maxExec, maxExecCnt = c, 1
+		} else if c == maxExec {
+			maxExecCnt++
+		}
+	}
+
+	if time := (maxExec-1)*(n+1) + maxExecCnt; time > len(tasks) {
+		return time
+	}
+	return len(tasks)
+}
